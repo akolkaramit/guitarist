4c73d46a59a44d92ec313f1278dfc0ad4000673d
AMIT AKOLKAR <akolkar.amit.445@gmail.com>
2020-05-04 03:35:22 +0530

    Update AboutUs.jsx

--- imports/web/AboutUs/AboutUs.jsx
+++ imports/web/AboutUs/AboutUs.jsx
@@ -1,20 +1,211 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import tambola from "tambola-generator";
+import { saveAsJpeg } from "save-html-as-image";
+import "./App.css";
 
-export default class AboutUs extends Component {
-    render(){
-        return(
-            <div className="guitar-aboutus-container container-fluid">
-                <h1 className="about-us text-center">
-                    Guitarist is Best Guitar Shop
-                </h1>
-                <div className="about-us-para">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia consequatur saepe ipsam culpa temporibus porro modi voluptas voluptatibus nisi, tempora ad sapiente autem commodi explicabo sit praesentium facilis ipsa placeat fugiat reiciendis. Voluptates ratione laborum rerum commodi ipsam! Consequuntur, inventore vel voluptatem officia excepturi facere fuga provident eaque harum quam, temporibus alias ea! Veniam tenetur ad fugit inventore modi, debitis officia maxime facere perferendis beatae, totam necessitatibus. Numquam minima doloribus assumenda provident cupiditate, laboriosam quidem necessitatibus quae, deleniti repellat distinctio expedita ad, totam quibusdam blanditiis possimus cum consectetur! Vero dolores voluptates vel nesciunt sint quo asperiores praesentium voluptatem placeat officiis fugit error numquam itaque id, iusto esse culpa veniam quia? Fugit iure ratione corporis quidem officia nulla et. Fugit sit consequuntur cum harum rem ex dignissimos quo facere minus. Earum est maiores dolorem provident rem quas error aperiam saepe, impedit iure veritatis architecto neque perferendis ut tempora pariatur odit deleniti explicabo. Esse adipisci excepturi in, explicabo deleniti ut velit placeat vel! Libero labore nisi, provident eum suscipit excepturi eaque ab placeat quod accusantium? Quasi voluptas facilis nobis velit. Quo tempore veritatis consectetur nobis earum iure ratione, ad voluptas cumque, iste dolore tenetur! Cumque quos minima similique dicta, cum sunt, hic quaerat porro rerum ducimus sapiente voluptates, minus dignissimos harum quibusdam quam nobis iure iusto quae commodi asperiores unde? Repellendus magni explicabo dolores sit fugit veritatis vero? Vitae quisquam, earum nobis odit laudantium in architecto omnis, ad magnam impedit minima, ipsam reprehenderit. Omnis sapiente, dolorum suscipit quam excepturi quasi, provident deleniti possimus ipsum doloribus perferendis dolores. Similique sit ut nam, iure dolor voluptatem temporibus rem qui odio? Explicabo esse corrupti culpa facilis voluptate, cupiditate ducimus ut nisi corporis sequi similique veniam commodi aspernatur ipsam unde, hic velit debitis illum quis consectetur! Voluptates enim doloremque modi repellat ullam quaerat perspiciatis voluptatibus fuga!
-                </div>
-                <blockquote className="blockquote text-right">
-                    <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                    <footer className="blockquote-footer">Someone <cite title="Source Title">Guitarist</cite></footer>
-                </blockquote>
-            </div>    
-        );
+class AboutUs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ticketCount: 4,
+      tickets: [],
+    };
+  }
+
+  generateTickets = (e) => {
+    e.preventDefault();
+    const value = this.state.ticketCount;
+    const tickets = tambola.getTickets(value);
+
+    this.setState({
+      tickets: tickets,
+    });
+  };
+
+  chnageInput = (e) => {
+    this.setState({
+      ticketCount: e.target.value,
+    });
+  };
+
+  downloadTicket = (ticketID) => {
+    let targetImageID = document.getElementById(`table${ticketID}`);
+
+    saveAsJpeg(targetImageID, {
+      filename: `Ticket${ticketID + 1}`,
+      printDate: true,
+    });
+  };
+
+  downloadAllTickets = () => {
+    const { length } = this.state.tickets;
+    for (let i = 0; i < length; i++) {
+      let targetImageID = document.getElementById(`table${i}`);
+      saveAsJpeg(targetImageID, {
+        filename: `Ticket${i + 1}`,
+        printDate: true,
+      });
     }
+  };
+
+  downloadChart = () => {
+    let targetImageID = document.getElementById("chart");
+
+    saveAsJpeg(targetImageID, {
+      filename: "Tickets",
+      printDate: true,
+    });
+  };
+
+  render() {
+    const { tickets, ticketCount } = this.state;
+
+    return (
+      <div className="tambola-body">
+        <h1 id="dfvfgfvhgfvh">Tambola Ticket Generator</h1>
+        <div className="tambola-wrapper">
+          <form className="tambola-form" onSubmit={this.generateTickets}>
+            <input
+              className="tambola-input"
+              type="text"
+              value={ticketCount}
+              onChange={this.chnageInput}
+            />
+            <button
+              className="tambola-btn"
+              type="submit"
+              value="Generate Tickets"
+            >
+              Generate Tickets
+            </button>
+            {tickets.length > 0 ? (
+              <React.Fragment>
+                <button
+                  className="tambola-btn"
+                  onClick={this.downloadAllTickets}
+                >
+                  Download all Tickets separately
+                </button>
+                <button className="tambola-btn" onClick={this.downloadChart}>
+                  Download Chart
+                </button>
+              </React.Fragment>
+            ) : null}
+          </form>
+          <div id="chart">
+            {tickets.map((data, index) => {
+              return (
+                <React.Fragment key={index}>
+                  <div className="tambola-table-margin">
+                    <div id={"table" + index}>
+                      <h3 className="ticket-name">Ticket {index + 1}</h3>
+                      <table key={index} className="tambola-table">
+                        <tbody className="tambola-tbody">
+                          {data.map((ticketRows, ind) => {
+                            return (
+                              <tr className="tambola-tr">
+                                <td
+                                  className={
+                                    ticketRows[0] === 0
+                                      ? "tambola-td-null"
+                                      : "tambola-td"
+                                  }
+                                >
+                                  {ticketRows[0] || null}
+                                </td>
+                                <td
+                                  className={
+                                    ticketRows[1] === 0
+                                      ? "tambola-td-null"
+                                      : "tambola-td"
+                                  }
+                                >
+                                  {ticketRows[1] || null}
+                                </td>
+                                <td
+                                  className={
+                                    ticketRows[2] === 0
+                                      ? "tambola-td-null"
+                                      : "tambola-td"
+                                  }
+                                >
+                                  {ticketRows[2] || null}
+                                </td>
+                                <td
+                                  className={
+                                    ticketRows[3] === 0
+                                      ? "tambola-td-null"
+                                      : "tambola-td"
+                                  }
+                                >
+                                  {ticketRows[3] || null}
+                                </td>
+                                <td
+                                  className={
+                                    ticketRows[4] === 0
+                                      ? "tambola-td-null"
+                                      : "tambola-td"
+                                  }
+                                >
+                                  {ticketRows[4] || null}
+                                </td>
+                                <td
+                                  className={
+                                    ticketRows[5] === 0
+                                      ? "tambola-td-null"
+                                      : "tambola-td"
+                                  }
+                                >
+                                  {ticketRows[5] || null}
+                                </td>
+                                <td
+                                  className={
+                                    ticketRows[6] === 0
+                                      ? "tambola-td-null"
+                                      : "tambola-td"
+                                  }
+                                >
+                                  {ticketRows[6] || null}
+                                </td>
+                                <td
+                                  className={
+                                    ticketRows[7] === 0
+                                      ? "tambola-td-null"
+                                      : "tambola-td"
+                                  }
+                                >
+                                  {ticketRows[7] || null}
+                                </td>
+                                <td
+                                  className={
+                                    ticketRows[8] === 0
+                                      ? "tambola-td-null"
+                                      : "tambola-td"
+                                  }
+                                >
+                                  {ticketRows[8] || null}
+                                </td>
+                              </tr>
+                            );
+                          })}
+                        </tbody>
+                      </table>
+                    </div>
+                    <button
+                      className="tambola-download-ticket-btn"
+                      onClick={() => this.downloadTicket(index)}
+                    >
+                      Download Ticket
+                    </button>
+                  </div>
+                </React.Fragment>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default App;
